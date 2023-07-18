37dcaa53489d3377d1d3d9177a9df98a2f4c0907
almoatasembellah <moatasembellah80@gmail.com>
2023-07-18 14:31:46 +0300

    added cors middleware to solve the CORS error.

--- app/Http/Middleware/OwnCors.php
+++ app/Http/Middleware/OwnCors.php
@@ -11,21 +11,12 @@ class OwnCors
 
     public function handle(Request $request, Closure $next): Response
     {
-        header("Access-Control-Allow-Origin: *");
-
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            return response('OK')
-                ->withHeaders($headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
     }
-    }
+}
 
